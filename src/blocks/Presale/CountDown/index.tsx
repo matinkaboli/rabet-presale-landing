4e958cbd29d305adbef5b9cc1de5b31408a875fb
mersedee <mersede.rahbar@gmail.com>
2021-12-29 16:33:14 +0330

    style layout

--- src/blocks/Presale/CountDown/index.tsx
+++ src/blocks/Presale/CountDown/index.tsx
@@ -6,13 +6,15 @@ import assetsSrc from 'public/images/buy-assets.png';
 
 import styles from './styles.module.scss';
 
+const classNames = require('classnames');
+
 const PresaleCountdown = () => (
-  <div className="row justify-center items-center">
-    <div className="col-5">
+  <div className={classNames('row justify-center items-center', styles.row)}>
+    <div className={classNames('col-lg-5 col-md-6 col-sm-12 col-xs-12', styles.col)}>
       <h1 className={styles.title}>RBT Pre-sale will be live in</h1>
       <DateCountdown end="12/29/2021 3:00:00 PM" />
     </div>
-    <div className="col-4">
+    <div className={classNames('col-lg-4 col-md-6 col-sm-12 col-xs-12', styles.col)}>
       <Image src={assetsSrc} height={250} width={484} />
     </div>
   </div>
